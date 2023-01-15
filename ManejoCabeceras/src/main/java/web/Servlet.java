c6ed2e33bcfcce1c7e5abf7555d8ed7a9474146c
AngelTravieso <angeltraviesoc@gmail.com>
2023-01-15 12:25:25 -0400

    ajustes servlet manejo headers

--- ManejoCabeceras/src/main/java/web/Servlet.java
+++ ManejoCabeceras/src/main/java/web/Servlet.java
@@ -8,40 +8,36 @@ import javax.servlet.http.*;
 @WebServlet("/ServletHeaders")
 public class Servlet extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         // setear tipo de contenido
         res.setContentType("text/html;charset=UTF-8");
 
-        PrintWriter out = res.getWriter();
+        try ( PrintWriter out = res.getWriter()) {
+            String metodoHttp = req.getMethod();
+            String uri = req.getRequestURI();
+            out.print("<html>");
+            out.print("<head>");
+            out.print("<title>Headers HTTP</title>");
+            out.print("</head>");
+            out.print("<body>");
+            out.print("<h1>Headers HTTP</h1>");
+            out.print(String.format("<p><b>Método HTTP:</b>%s</p>", metodoHttp));
+            out.print(String.format("<p><b>URI:</b> %s</p>", uri));
 
-        String metodoHttp = req.getMethod();
-        String uri = req.getRequestURI();
+            // Imprimir todos los headers disponibles
+            Enumeration headers = req.getHeaderNames();
+            while (headers.hasMoreElements()) {
+                // (String) headers.nextElement()
+                String nombreHeader = headers.nextElement().toString();
+                out.print(String.format("<p><b>%s</b>: %s</p>", nombreHeader, req.getHeader(nombreHeader)));
 
-        out.print("<html>");
-        out.print("<head>");
-        out.print("<title>Headers HTTP</title>");
-        out.print("</head>");
-        out.print("<body>");
+            }
 
-        out.print("<h1>Headers HTTP</h1>");
-        out.print(String.format("<p><b>Método HTTP:</b>%s</p>", metodoHttp));
-        out.print(String.format("<p><b>URI:</b> %s</p>", uri));
-
-        // Imprimir todos los headers disponibles
-        Enumeration headers = req.getHeaderNames();
-
-        while (headers.hasMoreElements()) {
-            // (String) headers.nextElement()
-            String nombreHeader = headers.nextElement().toString();
-            out.print(String.format("<p><b>%s</b>: %s</p>", nombreHeader, req.getHeader(nombreHeader)));
+            out.print("</body");
+            out.print("</html>");
 
         }
-
-        out.print("</body");
-        out.print("</html>");
-
-        // cerrar objeto printwriter
-        out.close();
 
     }
 }
